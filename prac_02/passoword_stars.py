a0bcea024df7433ea751bafcec40f320be38539f
Tan-Jiale <jiale.tan@my.jcu.edu.au>
2024-09-29 18:16:13 +0800

    Add prac 2 files

---
+++ prac_02/passoword_stars.py
@@ -0,0 +1,16 @@
+PASSWORD_LENGTH = 10
+
+def main():
+    """Display * by password length"""
+    password = validate_password()
+    print(len(password) * "*")
+
+def validate_password():
+    """Get validate password"""
+    password = input("Enter password: ")
+    while len(password) < PASSWORD_LENGTH:
+        print("Invalid password")
+        password = input("Enter password:")
+    return password
+
+main()
